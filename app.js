2af35d1809281da44f79931cf4a58eedbec40aaa
Dharmesh <dharmeshtiwari1280@gmail.com>
2025-03-28 11:25:39 +0530

    add global error handling

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddlware from "./middleware/error.middlware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddlware);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
