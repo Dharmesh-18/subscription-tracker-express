9bb951a781608ac6635bbd333da69310081500a4
Dharmesh <dharmeshtiwari1280@gmail.com>
2025-03-29 16:41:54 +0530

    add authorization for protected routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middleware/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.send({ title: "Get all User" }));
-userRouter.get("/:id", (req, res) => res.send({ title: "Get User details" }));
+userRouter.get("/", getUsers);
+
+userRouter.get("/:id", authorize, getUser);
 userRouter.post("/", (req, res) => res.send({ title: "Create new User" }));
 userRouter.put("/:id", (req, res) => res.send({ title: "Updates User" }));
 userRouter.delete("/:id", (req, res) => res.send({ title: "Delete a User" }));
